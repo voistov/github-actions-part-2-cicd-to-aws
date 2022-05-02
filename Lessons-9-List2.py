7be3bf214fffbc597781325671d83498b5c56a7d
voistov <93742263+voistov@users.noreply.github.com>
2022-05-02 21:00:33 +0300

    Update Lessons-9-List2.py

--- Lessons-9-List2.py
+++ Lessons-9-List2.py
@@ -1,35 +1,9 @@
-#          0    1       2       3       4
-cars = ['bmw', 'vw', 'seat', 'skoda', 'lada']
+from flask import Flask
+app = Flask(__name__)
 
-print(cars)
+@app.route("/")
+def hello():
+    return "Hello World!"
 
-for x in cars:
-    print(x.upper())
-
-for x in range(1, 6):
-   print(x)
-
-mynumber_list = list(range(0, 10))
-#print(mynumber_list)
-
-for x in mynumber_list:
-    x =x * 2
-    print(x)
-
-mynumber_list.sort(reverse=True)
-print(mynumber_list)
-
-print("Max number is:" + str(max(mynumber_list)))
-
-
-#          0    1       2       3       4
-cars = ['bmw', 'vw', 'seat', 'skoda', 'lada']
-
-mycars = cars[1:4]
-print(mycars)
-
-#          0    1       2       3       4
-cars = ['bmw', 'vw', 'seat', 'skoda', 'lada']
-
-mycars = cars[:] # [:] - do cloning current set
-
+if __name__ == "__main__":
+    app.run()
